982618e55ba9439003b7e65391a3fa1990c9d8fa
Elske Romeijn <elske.romeijn@gmail.com>
2022-03-13 21:30:47 +0100

    practice pages 4

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -103,7 +103,8 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
-      var chosenCategoryShortName = chooseRandomCategory(categories) + "";
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+      console.log(chosenCategoryShortName);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
